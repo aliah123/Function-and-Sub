cc6ce20a52974cea8492178379206ca2490a620d
Nur Aliah Husna Binti Mohamed Husaini <18DDT19F1098@student.ptss.edu.my>
2021-04-20 23:26:21 +0800

    create function and sub

--- Function and Sub/Form1.vb
+++ Function and Sub/Form1.vb
@@ -1,23 +1,39 @@
 ﻿Public Class Form1
-    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+
+    Private Sub X1()
         Lbl1.Text = "Data tersebut"
     End Sub
 
-    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        X1()
+    End Sub
+
+    Private Sub X2()
         Dim strText As String
         strText = TxtBox1.Text
         Lbl2.Text = strText
     End Sub
 
-    Private Function Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
-        Lbl3.Text = "Data tersebut"
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        X2()
+    End Sub
+
+    Private Function X3()
         Return Lbl3
     End Function
 
-    Private Function Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        Lbl3.Text = "Data tersebut"
+    End Sub
+
+    Private Function X4()
         Dim strText2 As String
         strText2 = TxtBox2.Text
         Lbl4.Text = strText2
         Return Lbl4
     End Function
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        X4()
+    End Sub
 End Class
